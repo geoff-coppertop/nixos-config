88d6aacb336832f047681984f0b7a7551185e7eb
Geoffrey Thomas <geoffrey.thomas@garmin.com>
2026-05-12 13:49:07 -0600

    Pull in desired VSCode extensions

--- users/thomasga/vscode.nix
+++ users/thomasga/vscode.nix
@@ -19,13 +19,33 @@ in {
       # Wrap existing settings into the default profile
       profiles.default = {
         extensions = with pkgs.vscode-extensions; [
+          github.vscode-github-actions
+          gruntfuggly.todo-tree
+          mhutchie.git-graph
+          ms-azuretools.vscode-containers
           ms-python.python
+          ms-vscode.cmake-tools
+          ms-vscode.cpp-devtools
           ms-vscode.cpptools
+          ms-vscode.cpptools-extension-pack
+          ms-vscode.cpptools-themes
+          ms-vscode-remote.remote-containers
         ];
         userSettings = {
           "files.autoSave" = "onFocusChange";
+          "git.blame.editorDecoration.enabled" = true;
           "terminal.integrated.defaultProfile.linux" = config.custom.cli.shell;
           "terminal.integrated.profiles.linux" = shellProfiles;
+          "todo-tree.general.tags": [
+            "BUG",
+            "HACK",
+            "FIXME",
+            "TODO",
+            "XXX",
+            "[ ]",
+            "[x]",
+            "//!! GT:"
+          ],
         };
       };
     };
